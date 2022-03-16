bfd9420f68143b954ff306e8ee24c1789921a757
yeabasas <yeabasayehgn@gmail.com>
2022-03-16 12:45:00 -0700

    Shapes

---
+++ Triangle.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Home
+ */
+public class Triangle {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+    
+            
+    
+}}    
+
